fb14c20e22e3fb58274fc0deadb75838c7697198
Phuc Tran <glup3.tran@gmail.com>
2025-05-15 18:30:49 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Hey GopherCon Europe 2025
+
+## Generate QR Code
+
+```sh
+cd project_root
+go run cmd/qr/main.go
+```
+
+## Resources
+
+- [QR Code Package](https://github.com/yeqown/go-qrcode)
+- [Go Logo](https://techicons.dev/icons/go)
---
+++ cmd/qr/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/yeqown/go-qrcode/v2"
+	"github.com/yeqown/go-qrcode/writer/standard"
+)
+
+func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
+	qr, err := qrcode.New("https://gophercon25eu.glup3.dev")
+	if err != nil {
+		return fmt.Errorf("create qrcode failed: %v", err)
+	}
+
+	options := []standard.ImageOption{
+		standard.WithLogoImageFilePNG("assets/gopher.png"),
+		standard.WithLogoSizeMultiplier(2),
+		standard.WithQRWidth(30),
+	}
+	writer, err := standard.New("static/qrcode.png", options...)
+	if err != nil {
+		return fmt.Errorf("create writer failed: %v", err)
+	}
+	defer writer.Close()
+
+	if err = qr.Save(writer); err != nil {
+		fmt.Printf("save qrcode failed: %v\n", err)
+	}
+
+	return nil
+}
